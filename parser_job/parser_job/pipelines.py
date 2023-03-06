d872e4e9fb20b6b341d60dd9aee076d117c62db5
kutsman <103063854+kutsman@users.noreply.github.com>
2023-03-07 02:56:13 +0300

    Update pipelines.py

--- parser_job/parser_job/pipelines.py
+++ parser_job/parser_job/pipelines.py
@@ -13,16 +13,17 @@ class ParserJobPipeline:
 
     def __init__(self):        
 
-      mongo_db = MongoClient()
-      self.db = mongo_db['vacancy_db']
+      mongo_db = MongoClient()                                        # подключаем mongo клиента
+      self.db = mongo_db['vacancy_db']                                # создает db
       
     def process_item(self, item, spider):
 
-      collection = self.db[spider.name]
-
-      if collection.find_one({'vacancy_link': item['vacancy_link']}):
-        collection.update_one(
-            {'vacancy_link': item['vacancy_link']}, 
+      collection = self.db[spider.name]                               # создаем коллекцию db[с названием имени паука], hh_ru or sj_ru
+ 
+      if collection.find_one({'vacancy_link': item['vacancy_link']}): # условие отсутствия дублирования в коллекции, если ссылка в коллекции уже есть
+                                                                      # обновляются все данные, но остается прежний id, если ссылки нет записывается новая вакансия
+        collection.update_one(                                         
+            {'vacancy_link': item['vacancy_link']},  
             {'$set': {
                 'name': item['name'],                          
                 'salary_min': item['salary_min'], 
@@ -31,12 +32,12 @@ class ParserJobPipeline:
               }
               )
       else:
-          collection.insert_one(item)
+          collection.insert_one(item) # если вакансии в коллекции нет, то просто вставляем ее
 
 
 
-      #collection.insert_one(item)
+      #collection.insert_one(item) # использовал для проверки 
       return item
       
-      #print(f'\n**********************\n{item}\n{spider}\n**********************\n')
+      #print(f'\n**********************\n{item}\n{spider}\n**********************\n') # использовал для проверки
       #return item
